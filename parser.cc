efe450c9d57c55da089228718e364ed94f0f8da0
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-01-28 19:26:43 -0800

    test

--- parser.cc
+++ parser.cc
@@ -7,9 +7,28 @@
 
 #define TOS ss.top()
 
-enum state : char { Q0, Q1 };
-enum symbol : char { EOS = '$', U = 'U', D = 'D', L = 'L', R = 'R', F = 'F', B = 'B', M = 'M', x = 'x', y = 'y' };
-enum dir : char { BLANK = NULL, CCW = '\'', ROT = '2' };
+enum state {
+	Q0, Q1
+};
+
+enum symbol {
+	EOS = '$',
+	  U = 'U',
+	  D = 'D',
+	  L = 'L',
+	  R = 'R',
+	  F = 'F',
+	  B = 'B',
+	  M = 'M',
+	  x = 'x',
+	  y = 'y'
+};
+
+enum dir : char {
+	BLANK = NULL,
+	CCW = '\'',
+	ROT = '2'
+};
 
 std::map<symbol, symbol> neg =
     {{ U, D },
@@ -20,8 +39,7 @@ std::map<symbol, symbol> neg =
      { B, F }};
 
 std::stack<char> ss;
-char *bufp;
-char save;
+char *tape, *arm1, *arm2;
 bool ON = false;
 
 char* append(const char* const out, const char& c) {
@@ -53,48 +71,58 @@ void error(const char* status) {
 }
 
 void parser(const char* const input) {
-    bufp = append(input, EOS);
-    bufp = strtok(bufp, " ");
+    tape = append(input, EOS);
+    tape = strtok(tape, " ");
+
+	arm1 = arm2 = tape;
+	printf("%s = %s = %s\n", arm2, arm1, tape);
 
     ss.push(EOS);
     ss.push(Q0);
 
-    while (!ss.empty() && bufp) {
+	int i = 0;
+
+	/* problem: accepts U (D D) and <odd offset> R R */
+    while (tape) {
         if (isterm(TOS)) {
-            if (TOS == bufp[0]) {
+            if (TOS == tape[0]) {
                 ss.pop();
-                bufp = strtok(NULL, " ");
+
+				arm2 = arm1;
+				arm1 = tape;
+                tape = strtok(NULL, " ");
+				printf("%s = %s = %s\n", arm2, arm1, tape);
+
+				if (i > 2) ON = true;
+				else ++i;
             }
             else error("unmatched token");
         }
         else {
             switch (TOS) {
                 case Q0 : {
-                    if (ON && bufp[0] == save) error("no layer change");
-
-                    ON = false;
-                    save = bufp[0];
+                    if (ON && tape[0] == *arm2) error("error -> no layer change");
 
                     ss.pop();
                     ss.push(Q0);
                     ss.push(Q1);
-                    ss.push(bufp[0]);
+                    ss.push(tape[0]);
                     break;
                 }
                 case Q1 : {
-                    if (bufp[0] == neg[(symbol)save]) {
-                        ON = true;
+					printf("---%s and %s\n", tape, arm1);
+                    if (tape != arm1) {
                         ss.pop();
-                        ss.push(bufp[0]);
+                        ss.push(tape[0]);
                     }
-                    else if (bufp[0] != save) {
+                    else if (tape[0] == neg[(symbol)(*arm2)]) {
                         ss.pop();
-                        ss.push(bufp[0]);
+                        ss.push(tape[0]);
                     }
-                    else error("duplicate token");
+                    else error("error -> duplicate token");
                     break;
                 }
-                default : error("unexpected variable");
+                default : error("error -> unexpected variable");
             }
         }
     }
@@ -104,7 +132,7 @@ void parser(const char* const input) {
 
 int main(int argc, char** argv) {
     if (argc == 2) parser(argv[1]);
-    else fprintf(stderr, "./%s <string>", argv[0]);
+    else fprintf(stderr, "./%s <string>\n", argv[0]);
 
     return 0;
 }
