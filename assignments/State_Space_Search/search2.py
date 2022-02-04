e8afb39770b813138e6900f961e497178edd97e9
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-02-04 10:29:01 -0800

    added state class for dfs

--- assignments/State_Space_Search/search2.py
+++ assignments/State_Space_Search/search2.py
@@ -14,31 +14,67 @@ START, FINAL = [0, 3, 2, 1], [1, 2, 3, 0]
 
 ACTION = ((1, 2), (0, 3), (3, 0), (2, 1))
 DIR = {
-    (0, 1): "R",
-    (0, 2): "D",
-    (1, 0): "L",
-    (1, 3): "D",
-    (2, 3): "R",
-    (2, 0): "U",
-    (3, 2): "L",
-    (3, 1): "U",
+    (0, 1): "Right",
+    (0, 2): "Down",
+    (1, 0): "Left",
+    (1, 3): "Down",
+    (2, 3): "Right",
+    (2, 0): "Up",
+    (3, 2): "Left",
+    (3, 1): "Up",
 }
 
 PATH = []
 MOVE = [None]
 
 
-def dfs(state, marker):
+class State:
+    def __init__(self, blank, parent, state, move):
+        self.blank = blank
+        self.parent = parent
+        self.state = state
+        self.next = None
+        self.move = move
+
+    def getMoves(self, curr):
+        while curr != None:
+            print("{} | {} -> ".format(curr.move, curr.state), end='')
+            curr = curr.next
+        print("goal")
+
+    def dfs(self, curr):
+        """depth-first search implementation"""
+        PATH.append(curr.state)
+
+        if curr.state != FINAL:
+            for move in ACTION[curr.blank]:
+                child = curr.state.copy()
+
+                child[curr.blank], child[move] = child[move], child[curr.blank]
+
+#                print(curr.state, " -> ", child)
+                if child not in PATH:
+                    node = State(move,
+                                 curr.state,
+                                 child,
+                                 DIR[(curr.blank, move)])
+                    curr.next = node # next pointer
+                    return self.dfs(node)
+
+        return None
+
+
+def dfs(state, blank):
     """depth-first search implementation"""
     PATH.append(state)
 
     if state != FINAL:
-        for move in ACTION[marker]:
+        for move in ACTION[blank]:
             child = state.copy()
-            child[marker], child[move] = child[move], child[marker]
+            child[blank], child[move] = child[move], child[blank]
 
             if child not in PATH:
-                MOVE.append(DIR[(marker, move)])
+                MOVE.append(DIR[(blank, move)])
                 return dfs(child, move)
 
     return None
@@ -70,6 +106,13 @@ def bfs():
 
 
 if __name__ == "__main__":
+    node = State(BLANK, None, START, None)
+    node.dfs(node)
+
+    print("DFS: ", end='')
+    node.getMoves(node)
+
+    """
     dfs(START, BLANK)
     for step, (node, action) in enumerate(zip(PATH, MOVE)):
         print("Step #{} State: {} Move: {}".format(step, node, action))
@@ -81,3 +124,4 @@ if __name__ == "__main__":
 
     for step, (node, action) in enumerate(zip(PATH, MOVE)):
         print("Step #{} State: {} Move: {}".format(step, node, action))
+    """
