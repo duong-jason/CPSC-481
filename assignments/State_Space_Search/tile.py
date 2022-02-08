9edd87bf18f48057251c4b65507cf2c1d4882617
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-02-08 09:29:30 -0800

    .

--- assignments/State_Space_Search/tile.py
+++ assignments/State_Space_Search/tile.py
@@ -4,7 +4,7 @@
 # 2022-02-02
 # @DannyDiep963, @duong-jason, @KonechyJ
 #
-# Assignment #1 - Tile Puzzle
+# Assignment #1 - TileBoard 
 #
 """tile puzzle implementation with DFS and BFS"""
 
@@ -13,32 +13,37 @@ class State:
     """State Representation: current configuration of puzzle"""
 
     def __init__(self, blank, state, move=None):
-        self.blank = blank
-        self.state = state
-        self.move = move
+        self.blank = blank # the blank tile position
+        self.state = state # a list of the current configuration
+        self.move = move   # the action/move that was performed to obtain the current state
 
 
-class Puzzle(State):
+class Board(State):
     """State Space Representation"""
 
     def __init__(self, blank, start=None, goal=None):
         State.__init__(self, blank, start)
 
+        # goal state
         self.goal = goal
+        # keeps track of the current path of states and actions/moves
         self.path = { 'state': [self.state], 'move': [None] }
+        # avoids revisited redundant nodes
         self.closed = [self.state]
-        self.size = len(self.state) ** (1 / 2) # get matrix size
+        # matrix size
+        self.size = len(self.state) ** (1 / 2)
 
     def reset(self):
         """re-initializes the path and closed list from future searches"""
         self.closed = [self.state]
-        self.path['state'] = [self.state]
-        self.path['move'] = [None]
+        self.path = { 'state': [self.state], 'move': [None] }
 
     def expand(self, blank):
         """expands neighbor tiles by removing tiles if the blank is on any border"""
+        # convert to numerical representation
         action = {"UP": -self.size, "DOWN": self.size, "LEFT": -1, "RIGHT": 1}
-        direction = ["UP", "RIGHT", "LEFT", "DOWN"]
+        # list of all possible directions
+        direction = ["DOWN", "UP", "RIGHT", "LEFT"]
 
         if blank < self.size:                   # blank is on the top-most border
             direction.remove("UP")
@@ -49,13 +54,14 @@ class Puzzle(State):
         if blank % self.size == self.size - 1:  # blank is on the right-most border
             direction.remove("RIGHT")
 
+        # returns the list of valid numerical actions/moves
         return [int(blank + action[move]) for move in direction]
 
     def action(self, blank, move):
         """returns the action with respect to the blank tile and the target tile"""
-        dis = move - blank  # find distance
-        action = {-self.size: "UP", self.size: "DOWN", -1: "LEFT", 1: "RIGHT"}
-        return action[dis]
+        dis = move - blank # find distance
+        action = {-self.size: "UP", self.size: "DOWN", -1: "LEFT", 1: "RIGHT"} # convert to string representation
+        return action[dis] # returns the action/move in a string representation
 
     def solve(self):
         """generates search algorithms to solve the tile puzzle"""
@@ -78,17 +84,25 @@ class Puzzle(State):
 
     def dfs(self, top):
         """depth-first search implementation"""
+
+        # validate if goal state reached
         if top.state != self.goal:
             for move in self.expand(top.blank):
+                # gets all possible actions from the current blank tile
+                # swaps a copy of the current blank tile with one of its child tile
+                # original copy is unmodified if backtrack occurs
                 child = top.state[:]
                 child[top.blank], child[move] = child[move], child[top.blank]
 
+                # check if the child has already been visited
                 if child not in self.closed:
+                    # child is now visited
                     self.closed.append(child)
 
                     self.path['state'].append(child)
                     self.path['move'].append(self.action(top.blank, move))
 
+                    # recursively search the child state until goal state or exhausted
                     return self.dfs(State(move, child, move))
 
         return None
@@ -96,17 +110,25 @@ class Puzzle(State):
     def bfs(self):
         """breadth-first search implementation"""
 
-        OPEN = [(self, [self.state], [None])]  # queue data structure + records the current path (state and move)
+        # queue data structure containing a tuple of the
+        # current state path and current action/move path
+        OPEN = [(self, [self.state], [None])]
 
         while OPEN:
             frontier, spath, mpath = OPEN[0][0], OPEN[0][1], OPEN[0][2]
 
             for move in self.expand(frontier.blank):
+                # gets all possible actions from the current blank tile
+                # swaps a copy of the current blank tile with one of its child tile
+                # original copy is unmodified for future swappings of the same parent
                 child = frontier.state[:]
                 child[frontier.blank], child[move] = child[move], child[frontier.blank]
 
+                # check if the child has already been visited
                 if child not in self.closed:
+                    # child is now visited
                     self.closed.append(child)
+                    # keeps track of the current path from root to child state
                     OPEN.append((State(move, child, move),
                                  spath + [child],
                                  mpath + [self.action(frontier.blank, move)]))
@@ -120,8 +142,8 @@ class Puzzle(State):
 
 
 if __name__ == "__main__":
-    TILE_2 = Puzzle(0, [0, 3, 2, 1], [1, 2, 3, 0])
-    TILE_3 = Puzzle(4, [1, 4, 3, 7, 0, 6, 5, 8, 2], [1, 2, 3, 4, 5, 6, 7, 8, 0])
+    TILE_2 = Board(0, [0, 3, 2, 1], [1, 2, 3, 0])
+    TILE_3 = Board(4, [1, 4, 3, 7, 0, 6, 5, 8, 2], [1, 2, 3, 4, 5, 6, 7, 8, 0])
 
     TILE_2.solve()
     TILE_3.solve()
