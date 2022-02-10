8a8430f5533a6b4fa86de4e6d54f39dc96165d8e
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-02-10 08:30:17 -0800

    Test

--- assignments/State_Space_Search/tile.py
+++ assignments/State_Space_Search/tile.py
@@ -13,9 +13,8 @@ class State:
     """State Representation: current configuration of puzzle"""
 
     def __init__(self, blank, state, move=None):
-        self.blank = blank # the blank tile position
-        self.state = state # a list of the current configuration
-        self.move = move   # the action/move that was performed to obtain the current state
+        self.blank = blank  # the blank tile position
+        self.state = state  # a list of the current configuration
 
 
 class Board(State):
@@ -27,7 +26,7 @@ class Board(State):
         self.goal = goal                             # goal state
         self.closed = [self.state]                   # avoids revisited (cycles)
         self.table = table                           # set of possible moves
-        self.size = int(len(self.state) ** (1 / 2))  # matrix size
+        self.size = len(self.state) ** (1 / 2)       # matrix size
         self.path = {                                # keeps track of the current path of states and actions/moves
             "state": [self.state],
             "move" : [None]
@@ -43,12 +42,10 @@ class Board(State):
 
     def action(self, blank, move):
         """returns the action with respect to the blank tile and the target tile"""
-        # find distance
-        diff = move - blank
-        # convert to string representation
+        # map of numerical position to string position
         action = { -self.size: "UP", self.size: "DOWN", -1: "LEFT", 1: "RIGHT" }
-
-        return action[diff] # returns the action/move in a string representation
+        # returns the action/move in a string representation based on the direction
+        return action[move - blank]
 
     def solve(self):
         """generates search algorithms to solve the tile puzzle"""
@@ -69,27 +66,27 @@ class Board(State):
 
         print("\nExplored {} States".format(len(self.closed)))
 
-    def dfs(self, top, path):
+    def dfs(self, frontier, path):
         """depth-first search implementation"""
 
         # validate if goal state reached
-        if top.state != self.goal:
-            for move in self.table[top.blank]:
+        if frontier.state != self.goal:
+            for move in self.table[frontier.blank]:
                 # gets all possible actions from the current blank tile
                 # swaps a copy of the current blank tile with one of its child tile
                 # original copy is unmodified if backtrack occurs
-                child = top.state[:]
-                child[top.blank], child[move] = child[move], child[top.blank]
+                child = frontier.state[:]
+                child[frontier.blank], child[move] = child[move], child[frontier.blank]
 
                 # check if the child has already been visited
                 if child not in self.closed and child not in path:
-                    # child is now visited
-                    self.closed.append(child)
+                    self.closed.append(child) # child is now visited
 
                     self.path["state"].append(child)
-                    self.path["move"].append(self.action(top.blank, move))
+                    self.path["move"].append(self.action(frontier.blank, move))
 
                     # recursively search the child state until goal state or exhausted
+                    # keeps a record of the current path (root + sibilings)
                     return self.dfs(State(move, child, move), path + [child])
 
         return None
@@ -98,13 +95,13 @@ class Board(State):
         """breadth-first search implementation"""
 
         # queue data structure
-        Open = [self]
+        open = [self]
         # tuple-list conntaining the current state path and current action/move path
-        Mem = [([self.state], [None])]
+        mem = [([self.state], [None])]
 
-        while Open:
-            frontier = Open.pop(0)
-            spath, mpath = Mem[0][0], Mem[0][1]
+        while open:
+            frontier = open.pop(0)
+            spath, mpath = mem[0][0], mem[0][1]
 
             for move in self.table[frontier.blank]:
                 # gets all possible actions from the current blank tile
@@ -115,25 +112,23 @@ class Board(State):
 
                 # check if the child has already been visited (cycle)
                 # or currently in queue to be explored (redundant-path)
-                if child not in self.closed and child not in Open:
-                    # child is now visited
-                    self.closed.append(child)
-                    # push child node to explore set
-                    Open.append(State(move, child, move))
-                    # keeps track of the current path from root to child state
-                    Mem.append((
+                if child not in self.closed and child not in open:
+                    self.closed.append(child)             # child is now visited
+                    open.append(State(move, child, move)) # push child node to explore set
+                    mem.append((                          # keeps track of the current path from root to child state
                         spath + [child],
                         mpath + [self.action(frontier.blank, move)]
                     ))
 
+                # validates whether the current state has reached its goal state
                 if child == self.goal:
-                    self.path = {
+                    self.path = { # save the direct paths and moves
                         "state": spath + [child],
                         "move" : mpath + [self.action(frontier.blank, move)],
                     }
                     return None
 
-            Mem.pop(0)
+            mem.pop(0)
 
 
 if __name__ == "__main__":
